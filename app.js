05fb0bdd8bc483874dd173c61684c768e248923d
Elihu Chitrit <eli@qwamo.com>
2023-09-20 19:48:27 +0300

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.use(express.urlencoded());
 app.use(express.json());
 
 app.use('/', (req,res) => {
-    return res.status(200).json({ message: "Hello from AWS Pipeline and Beanstalk"});
+    return res.status(200).json({ message: "Hello from AWS"});
 })
 
 app.listen(3001);
